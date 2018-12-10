92e3bf198d405f331e8e45a07d37af0ad7674712
Mohammed Shoeb <mohammed.shoeb@mountblue.io>
2018-12-10 18:31:43 +0530

    Working code

--- index.js
+++ index.js
@@ -12,11 +12,48 @@ function fakeAjax(url,cb) {
 		cb(fake_responses[url]);
 	},randomDelay);
 }
+// let printed={};
+let printed={"The first text":2,
+							"The middle text":2,
+							"The last text":2};
 
 function getFile(file) {
 	fakeAjax(file,function(text){
-		console.log(text);
 		// what do we do here?
+		if(text=="The first text"){
+			console.log("The first text");
+			printed[text]=0;
+			if(printed["The middle text"]==1){
+				console.log("The middle text");
+				printed["The middle text"]=0;
+				if(printed["The last text"]==1){
+					console.log("The last text");
+					printed["The last text"]=0;
+				}
+			}
+		}
+		else if(text=="The middle text"){
+			if(printed["The first text"]==2){
+				printed["The middle text"]=1;
+			}
+			else{
+				console.log("The middle text");
+				printed["The middle text"]=0;
+				if(printed["The last text"]==1){
+					console.log("The last text");
+					printed["The last text"]==0;
+				}
+			}
+		}
+		else{
+			if(printed["The middle text"]==0){
+				console.log(text);
+				printed["The last text"]=0;
+			}
+			else {
+				printed["The last text"]=1;
+			}
+		}
 	});
 }
 
