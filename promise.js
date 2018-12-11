be37791889a9f92fe94edf8ba8e7fe7365dec302
Mohammed Shoeb <mohammed.shoeb@mountblue.io>
2018-12-11 15:54:16 +0530

    Add Promise project file

---
+++ promise.js
@@ -0,0 +1,34 @@
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function getFile(file) {
+	let p=new Promise((resolve,reject)=>{
+		fakeAjax(file,(txt)=>resolve(txt))
+	});
+	return p;
+}
+let p1=getFile("file1");
+let p2=getFile("file2");
+let p3=getFile("file3");
+
+p1.then((txt)=>{
+	console.log(txt);
+	return p2;
+}).then((txt)=>{
+	console.log(txt);
+	return p3;
+}).then((txt)=>{
+	console.log(txt)
+})
